e14ab18e7d7261666db33fcde2aa0ae2ae9e26fe
yangpan12 <738912168@qq.com>
2020-10-09 11:25:43 +0800

    feat:hello world

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Hello from './Hello';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Hello message="Hello World2"/>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
