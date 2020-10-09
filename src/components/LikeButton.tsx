d6a99112adba90c49ec55d8a2b0d67e7e8c0c459
yangpan12 <738912168@qq.com>
2020-10-09 14:10:08 +0800

    feat:useState Multiple

--- src/components/LikeButton.tsx
+++ src/components/LikeButton.tsx
@@ -1,14 +1,15 @@
 import React, {useState} from 'react';
 
 const LikeButton: React.FC = () => {
-  const [obj, setObj] = useState({like: 0, on: true});
+  const [like, setLike] = useState(0);
+  const [on, setOn] = useState(true);
   return (
     <>
-      <button onClick={()=>{setObj({like: obj.like + 1, on: obj.on})}}>
-        {obj.like} 🤹‍♂️
+      <button onClick={()=>{setLike(like + 1)}}>
+        {like} 🤹‍♂️
       </button>
-      <button onClick={()=>{setObj({like: obj.like, on: !obj.on})}}>
-        {obj.on ? 'ON' : 'OFF'}
+      <button onClick={()=>{setOn(!on)}}>
+        {on ? 'ON' : 'OFF'}
       </button>
     </>
   )
