243ea0a1c8d78958d1155b0b51dd1b94c44574d7
yangpan12 <738912168@qq.com>
2020-10-10 11:29:31 +0800

    feat:改造hoc成自定义hook的方式

--- src/App.tsx
+++ src/App.tsx
@@ -2,32 +2,25 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import LikeButton from './components/LikeButton';
-import withLoader from './components/withLoader';
+import useURLLoader from './hooks/useURLLoader'
 
 interface IShowResult {
   message: string;
   status: string;
 }
-const DogShow: React.FC<{data: IShowResult}> = ({data}) =>{
-  return (
-    <>
-      <h2>Dog show: {data.status}</h2>
-      <img src={data.message}/>
-    </>
-  )
-}
-
 function App() {
   const [ show, setShow ] = useState(true);
-  const WrappedDogShow = withLoader(DogShow, 'https://dog.ceo/api/breeds/image/random')
+  const [data, loading] = useURLLoader('https://dog.ceo/api/breeds/image/random', [show]);
+  const dogResult = data as IShowResult;
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />  
         <p>
-          <button onClick={()=>{setShow(!show)}}>Toggle Tracker</button>
+          <button onClick={()=>{setShow(!show)}}>Refresh dog photo</button>
         </p>
-        <WrappedDogShow/>
+        { loading ? <p>狗狗读取中</p>
+        :<img src={dogResult && dogResult.message}/> }
         <LikeButton/>
         <a
           className="App-link"
